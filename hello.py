1bb5e3d2abe5bf978cca5ff340325ca49c7b68d1
Mahmoud <mga887@nyu.edu>
2022-12-23 23:03:58 -0500

    FLASK WTF

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no is supposed to know"
+
+# Create a Form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator 
 @app.route('/')
@@ -28,6 +38,19 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
+
+
 
 """
 Filters:
@@ -38,5 +61,14 @@ upper
 title
 trim
 striptags
+
+Three steps to push to git from terminal
+git add .
+git commit -am "message"
+git push
+
+To initiliaze version control go to:
+https://codemy.com/git
 """
+
 
